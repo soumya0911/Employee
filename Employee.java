73f27f5cf3549fa13c086c26d7a667ca6cb9c35b
soumya0911 <suryavanshisoumya5@gmail.com>
2025-05-06 14:51:51 +0530

    save this file

--- Employee.java
+++ Employee.java
@@ -2,19 +2,22 @@ class Employee{
     String name;
     int id;
     double salary;
-    public Employee(String name,int id,double salary){
+    int experience;
+    public Employee(String name,int id,double salary,int experience){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.experience=experience;
     }
     public void displayInfo(){
         System.out.println("Employee ID:"+id);
         System.out.println("Employee Name:"+name);
         System.out.println("Employee Salary:$"+salary);
+        System.out.println("Employee exxperience:"+experience);
     }
     public static void main(String[]args){
         //creating an oject of employee
-        Employee emp1=new Employee("Soumya",101,150000);
+        Employee emp1=new Employee("Soumya",101,150000,5);
         emp1.displayInfo();
     }
 }
